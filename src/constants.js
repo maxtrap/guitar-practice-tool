9f196f3030fb9801ff6c7a7bef43963f10f77118
Max Trapido <maxtrapido@gmail.com>
2024-03-19 12:47:46 -0700

    Make constants immutable

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 
-export const COLORS = {
+export const COLORS = Object.freeze({
     PRIMARY: '#d465fc',
     SECONDARY: '#a22dff',
     TERTIARY: '#e51ad2',
@@ -12,21 +12,20 @@ export const COLORS = {
     PRIMARY_DARK: '#662f79',
     SECONDARY_DARK: '#541f7c',
     FONT_COLOR_DARK: '#575757',
-}
+})
 
-export const TEMPO_RANGE = {
+export const TEMPO_RANGE = Object.freeze({
     MIN_TEMPO: 30,
     MAX_TEMPO: 280
-}
+})
 
 export const NOTIF_TYPES = Object.freeze({
     NONE: "",
-    TEXT_ONLY: "Please enter an integer value",
     OUT_OF_RANGE: `Tempo must be between ${TEMPO_RANGE.MIN_TEMPO} and ${TEMPO_RANGE.MAX_TEMPO} beats per minute`,
     NOT_A_NUMBER: "Tempo must be a number"
 })
 
-export const BUTTON_TYPES = {
+export const BUTTON_TYPES = Object.freeze({
     PLUS: 1,
     MINUS: 2
-}
+})
